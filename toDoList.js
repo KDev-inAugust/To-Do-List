afb2932c8a89713e1f0c1ec0e0a7b0f10e0e4626
Kane Mathis <admin@kanemathis.com>
2022-04-22 00:24:16 -0400

    mock server post working'

--- toDoList.js
+++ toDoList.js
@@ -3,15 +3,42 @@ document.addEventListener("DOMContentLoaded", ()=>{
     document.querySelector('form').addEventListener('submit', (e)=>{
         e.preventDefault();
         buildToDo(inputText.value)
+        
     })
 })
 
 function buildToDo(todo){
-    let p = getElementById('tasks')
+    let li = document.createElement('li');
     let btn = document.createElement('button');
     btn.addEventListener('click', handleDelete);
+    li.textContent=`${todo}`;
+    document.getElementById('tasks').appendChild(li);
+    btn.innerText='x';
+    li.appendChild(btn);
+    postTask(todo);
 }
 
-function handleDelete(){
-    
+
+
+function handleDelete(e){
+e.target.parentNode.remove();
 }
+
+
+function postTask(task){
+    let taskObject ={
+        Task:task,
+        dateAdded:"test string",
+    }
+fetch('http://localhost:3000/ToDoList', {
+    method: 'POST',
+    headers:{
+        'Content-Type':'application/json'
+    },
+    body:JSON.stringify(taskObject)
+})
+.then(res => res.json())
+.then(task => console.log(task))
+
+}
+
